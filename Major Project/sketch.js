f4ace3b88f45240ce88baf9ee02aeef64bdfb8d5
Liu1301430 <1301430@sps-student.sk.ca>
2018-05-11 15:19:50 -0600

    Update

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -1,12 +1,16 @@
 // Moving Square
 // Catherine Liu
 // Feb 26, 2018
+
+let check;
 let currentLocation;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
-  currentLocation = geoCheck();
+  check = geoCheck();
+  currentLocation = getCurrentPosition();
 }
