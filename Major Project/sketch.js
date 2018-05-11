2fda538c9c5c4ac9ded52e7cd17aa57f00ce0aa2
Liu1301430 <1301430@sps-student.sk.ca>
2018-05-11 14:56:17 -0600

    Update

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -1,24 +1,12 @@
 // Moving Square
 // Catherine Liu
 // Feb 26, 2018
-
-let blockColours = [[255, 0, 0], [0, 179, 0], [26, 26, 255], [255, 255, 0]];
-let squareNumber = 0;
-
+let currentLocation;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-
 }
 
 function draw() {
-
-
-  for (let i = width / 2 - 100; i < 700; i += 120) {
-    for (let j = height / 2 - 100; j < 320; j += 120) {
-      fill(blockColours[squareNumber][0], blockColours[squareNumber][1], blockColours[squareNumber][2]);
-      rect(i, j, 100, 100);
-      squareNumber += 1;
-    }
-  }
+  background(255);
+  currentLocation = geoCheck();
 }
