5ca6742c8037165e1b61994e9362f05732856c14
CsabaNemeth <36053905+CsabaNemeth@users.noreply.github.com>
2018-05-14 15:17:46 -0600

    Finished Pathfinding Algorithm
    -Adjacency Matrix representing a nodal network
    -Function returns an array with node numbers as the path to take

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -1,16 +1,81 @@
-// Moving Square
-// Catherine Liu
-// Feb 26, 2018
+//Node-Based Graph
+// The adjacency matrix defining the graph.
 
-let check;
-let currentLocation;
+let testPath, constructedPath;
+let finishedPath;
+
+let _ = Infinity; //No path connecting nodes;
+let nodeNetwork = [ //Index starts at 0;
+  [ _, 6, 5, _],
+  [ 6, _, _, 5],
+  [ 5, _, _, 9],
+  [ _, 5, 9, _],
+];
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  finishedPath = findPath(nodeNetwork, 0, 3);
+  print(finishedPath);
 }
 
-function draw() {
-  background(255);
-  check = geoCheck();
-  currentLocation = getCurrentPosition();
+function shortestPath(matrix, startVertex) {
+
+  //Creates three arrays with length equal to matrix
+  let done = new Array(matrix.length);
+  let pathLengths = new Array(matrix.length);
+  let predecessors = new Array(matrix.length);
+
+  done[startVertex] = true;
+
+  //Loops through matrix[startVertex] and writes the values into the pathLengths array
+  for (let i = 0; i < matrix.length; i++) {
+    pathLengths[i] = matrix[startVertex][i];
+    if (matrix[startVertex][i] !== Infinity) {
+      predecessors[i] = startVertex;
+    }
+  }
+
+  //Length from node to itself is 0;
+  pathLengths[startVertex] = 0;
+
+  for (let i = 0; i < matrix.length - 1; i++) {
+    let closest = -1;
+    let closestDistance = Infinity;
+    for (let j = 0; j < matrix.length; j++) {
+      if (!done[j] && pathLengths[j] < closestDistance) {
+        closestDistance = pathLengths[j];
+        closest = j;
+      }
+    }
+
+    done[closest] = true;
+
+    for (let j = 0; j < matrix.length; j++) {
+      if (!done[j]) {
+        let possiblyCloserDistance = pathLengths[closest] + matrix[closest][j];
+        if (possiblyCloserDistance < pathLengths[j]) {
+          pathLengths[j] = possiblyCloserDistance;
+          predecessors[j] = closest;
+        }
+      }
+    }
+  }
+  return { "startVertex": startVertex,
+    "distances": pathLengths,
+    "tree": predecessors };
+}
+
+function constructPath(object, endVertex) {
+  let path = [];
+  do {
+    path.unshift(endVertex);
+    endVertex = object.tree[endVertex];
+  }
+  while (endVertex !== object.startVertex);
+  return path;
+}
+
+function findPath(matrix, startNode, endNode) {
+  return constructPath(shortestPath(matrix, startNode), endNode);
 }
