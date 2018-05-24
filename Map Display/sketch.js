942ea695b61e180efc2bcfbda18b455fefe65b85
Liu1301430 <1301430@sps-student.sk.ca>
2018-05-24 14:32:04 -0600

    Added Map Displaying Code

---
+++ Map Display/sketch.js
@@ -0,0 +1,41 @@
+// Moving Square
+// Catherine Liu
+// Feb 26, 2018
+
+let map1, map2, map3;
+let transferArray = [];
+let nodeArray = [];
+let counter;
+
+
+function preload(){
+  map1 = loadImage("images/floor1.png");
+  map2 = loadImage("images/floor2.png");
+  map3 = loadImage("images/floor3.png");
+
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if (map2.width < map2.height) {
+    image(map2, 0, 0, map2.height/2.5, map2.width/2.5);
+  }
+  else {
+    image(map2, 0, 0, map2.width/2.5, map2.height/2.5);
+  }
+
+  counter = 1;
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    transferArray.push(counter, mouseX, mouseY); //counter is node number
+    nodeArray.push(transferArray);
+    counter += 1;
+    fill(0);
+    ellipse(mouseX, mouseY, 5, 5);
+    transferArray = [];
+    mouseIsPressed = false;
+  }
+}
