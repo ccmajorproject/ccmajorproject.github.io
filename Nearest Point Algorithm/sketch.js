f4e7370f47ab0f606a22e4ef646dc3a9fd78a435
Liu1301430 <1301430@sps-student.sk.ca>
2018-05-28 15:19:49 -0600

    Finished: Determines user location

--- Nearest Point Algorithm/sketch.js
+++ Nearest Point Algorithm/sketch.js
@@ -5,24 +5,33 @@
 //return the index of the coordinate nearest to the input coordinate
 
 let array = [[1, 2,], [20,400,], [43,67,], [12,43,], [6,5,], [100,100,], [234,654,], [321,452,], [120,120,],];
-
+let hypArray = [];
+let smallestValue, position;
 
 function setup() {
-  createCanvas(100, 100);
+  createCanvas(500, 500);
 }
 function draw() {
-
+  if (mouseIsPressed) {
+    console.log(mouseX, mouseY);
+    console.log(nearestNode(array, [mouseX, mouseY]));
+    console.log(array[nearestNode(array, [mouseX, mouseY])]);
+    mouseIsPressed = false;
+  }
 }
 
 function nearestNode(array, coordinate) {
-  let hypArray = [];
   for (let i = 0; i < array.length; i++) {
     let xDistance = abs(array[i][0] - coordinate[0]);
     let yDistance = abs(array[i][1] - coordinate[1]);
-    let hyp = sqrt(sq(xDistance) + sq(yDistance));
+    let hyp = floor(sqrt(sq(xDistance) + sq(yDistance)));
 
     hypArray.push(hyp);
   }
 
-  return hypArray.indexOf(Math.min(hypArray)) + 1;
+  smallestValue = Math.min.apply(null, hypArray);
+  position = hypArray.indexOf(smallestValue);
+  hypArray = [];
+
+  return position;
 }
